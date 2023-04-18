be5fcf740ac3ddd6639ded84211a2952b3bd2b83
beuleu <billyklotz789@gmail.com>
2023-04-18 19:37:28 +0200

    white page solidJs ts client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import style from './App.module.css'
+
+const App: Component = () => {
+  return (
+      <>
+        <div class={style.app}></div>
+      </>
+  )
+};
+
+export default App;

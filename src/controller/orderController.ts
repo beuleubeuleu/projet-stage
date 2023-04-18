84b19ec15545ed4c666e0064e2b2bced94c92440
beuleu <billyklotz789@gmail.com>
2023-04-18 12:57:31 +0200

    getAllOrders

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express"
-import Order from "../models/orderModel"
+import Order                 from "../models/orderModel"
+import { IOrder }            from "../interfaces/InterfaceOrder";
 
 export const getAllOrders = async (req: Request, res: Response) => {
-
+  try {
+    const orders: IOrder[] = await Order.find().populate("orderItems")
+    res.status(200).json({ orders })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "internal server error" })
+  }
 }
 
 export const getOneOrder = async (req: Request, res: Response) => {

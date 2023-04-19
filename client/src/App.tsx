5965a3b506614bf19f6dc59c756811f22d9e9860
beuleu <billyklotz789@gmail.com>
2023-04-19 14:55:46 +0200

    add the cute toaster

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import style from "./App.module.css"
 import React from "react";
 import { Header }      from "./components/Header";
 import { ProductList } from "./components/ProductList";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
 
   return (
       <div className={style.App}>
+        <Toaster/>
         <Header/>
         <ProductList/>
       </div>

e067ca811d7463d1326b0d9630123358be044dfa
beuleu <billyklotz789@gmail.com>
2023-04-19 17:40:40 +0200

    loading css

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,16 +1,21 @@
-import useFetch     from "../hooks/useFetch"
-import style        from "./ProductList.module.css"
-import { IProduct } from "../interface/IProduct";
+import useFetch        from "../hooks/useFetch"
+import style           from "./ProductList.module.css"
+import { IProduct }    from "../interface/IProduct";
 import { ProductCard } from "./ProductCard";
 
 export const ProductList = () => {
   const { data, loading, error } = useFetch("/api/v1/products")
   return (
       <>
-        { loading && <div>A moment please...</div> }
-        { error && ( <div>{ `There is a problem fetching the post data - ${ error }` }</div> ) }
+        { loading && <div className={ style.ldsEllipsis }>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div> }
+        { error && ( <div>{ `There is a problem fetching the products data - ${ error }` }</div> ) }
         <ul className={ style.productList }>
-          { data && data.map((product: IProduct) => <ProductCard product={ product } key={product._id}/>) }
+          { data && data.map((product: IProduct) => <ProductCard product={ product } key={ product._id }/>) }
         </ul>
       </>
 
